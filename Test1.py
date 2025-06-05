9b94cdfa4b4ec11b2b1fef0f60e54c9d2e33a448
FrogBeto <albertoazevedo112@gmail.com>
2025-06-05 20:13:07 -0300

    Testes para lista de projetos

--- Test1.py
+++ Test1.py
@@ -8,7 +8,6 @@ class ListaDeRepositorios():
 
     def requisicao_api(self):
         resposta = requests.get(
-            #f'https://api.github.com/users/{self._usuario}/repos')
             f'https://api.github.com/repos/{self._usuario}/contributors?page={self._valor}')
 
         if resposta.status_code == 200:
@@ -17,33 +16,32 @@ class ListaDeRepositorios():
             return resposta.status_code
 
     def imprime_repositorios(self):
-        #f = open("logins.txt", "x")
+        #f = open("usuarios.txt", "x")
         dados_api = self.requisicao_api()
 
         if type(dados_api) is not int:
-            with open("logins.txt", "w") as f:
-                for j in range(14):
+            with open("usuarios.txt", "w") as f:
+                for j in range(15):
                     self._valor = j + 1
                     dados_api = self.requisicao_api()
                     for i in range(len(dados_api)):
                         f.write(dados_api[i]['login'] + " " + str(dados_api[i]['contributions']) + "\n")
 
+                    if len(dados_api) == 0:
+                        print("Nenhum login foi encontrado na lista!")
+
             # open and read the file after the overwriting:
-            with open("logins.txt") as f:
+            with open("usuarios.txt") as f:
                 print(f.read())
-
-            #for i in range(len(dados_api)):
-             #   print(dados_api[i]['login'])
-
-        #._valor = 14
-        #dados_api = self.requisicao_api()
-        #if type(dados_api) is not int:
-            #for i in range(len(dados_api)):
-                #print(dados_api[i]['login'])
 
         else:
             print(dados_api)
 
 
-repositorios = ListaDeRepositorios('arendst/Tasmota')
-repositorios.imprime_repositorios()
+#repositorios = ListaDeRepositorios('arendst/Tasmota')
+#repositorios.imprime_repositorios()
+#https://api.github.com/repos/arendst/Tasmota/contributors?page=1
+
+#with open("projetos.csv") as f:
+ # for x in f:
+  #  print(x)
