6eb25dd69e68ef365558f665b8bda203c1e637ee
FrogBeto <albertoazevedo112@gmail.com>
2025-06-04 22:12:51 -0300

    Consulta de todas as páginas

--- Test1.py
+++ Test1.py
@@ -4,7 +4,7 @@ import json
 class ListaDeRepositorios():
     def __init__(self, usuario):
         self._usuario = usuario
-        self._valor = 13
+        self._valor = 0
 
     def requisicao_api(self):
         resposta = requests.get(
@@ -17,10 +17,23 @@ class ListaDeRepositorios():
             return resposta.status_code
 
     def imprime_repositorios(self):
+        f = open("logins.txt", "x")
         dados_api = self.requisicao_api()
+
         if type(dados_api) is not int:
-            for i in range(len(dados_api)):
-                print(dados_api[i]['login'])
+            with open("logins.txt", "a") as f:
+                for j in range(14):
+                    self._valor = j + 1
+                    dados_api = self.requisicao_api()
+                    for i in range(len(dados_api)):
+                        f.write(dados_api[i]['login'] + "\n")
+
+            # open and read the file after the overwriting:
+            with open("logins.txt") as f:
+                print(f.read())
+
+            #for i in range(len(dados_api)):
+             #   print(dados_api[i]['login'])
 
         #._valor = 14
         #dados_api = self.requisicao_api()
