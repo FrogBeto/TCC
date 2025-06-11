068dd7c5951b7f602684b8f1d1ac75fa1b5b90ea
FrogBeto <albertoazevedo112@gmail.com>
2025-06-11 16:59:40 -0300

    Em construção

--- Teste2.py
+++ Teste2.py
@@ -1,16 +1,27 @@
 import csv
 import pandas as pd
+import requests
+import json
 
 df = pd.read_csv('teste.csv')
-donos = []
+dono = []
 projeto = []
 
 #print(df['Dono'])
 for x in df['Dono']:
-    donos.append(x)
+    dono.append(x)
 for x in df['Projeto']:
     projeto.append(x)
 
-for x in range(len(donos)):
-    print(donos[x] + " " + projeto[x])
+for x in range(len(dono)):
+    print(dono[x] + " " + projeto[x])
 
+class Busca():
+    def __init__(self, dono, projeto):
+        self.dono = dono
+        self.projeto = projeto
+
+    def requisicao(self):
+        reposta = requests.get(
+            f'https://api.github.com/repos/{}'
+        )
