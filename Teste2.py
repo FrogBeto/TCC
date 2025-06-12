bc2bce3d8e936cfa81ce96e95c392cab21cb9d4c
FrogBeto <albertoazevedo112@gmail.com>
2025-06-12 15:45:23 -0300

    Testes finais

--- Teste2.py
+++ Teste2.py
@@ -1,27 +1,55 @@
-import csv
 import pandas as pd
 import requests
 import json
 
 df = pd.read_csv('teste.csv')
-dono = []
-projeto = []
+donos = []
+projetos = []
 
-#print(df['Dono'])
+token = ""
+headers = {
+    "Authorization": "token {}".format(token)
+}
+
+#print(df)
 for x in df['Dono']:
-    dono.append(x)
+    donos.append(x)
 for x in df['Projeto']:
-    projeto.append(x)
+    projetos.append(x)
 
-for x in range(len(dono)):
-    print(dono[x] + " " + projeto[x])
+#for x in range(len(dono)):
+    #print(dono[x] + " " + projeto[x])
 
-class Busca():
+#f = open("usuarios.csv", "a")
+
+#f.write("aaaaaa,44,55555\n")
+#f.write(dono[0] + "," + str(3) + "," + projeto[0] + "\n")
+
+class Busca:
     def __init__(self, dono, projeto):
         self.dono = dono
         self.projeto = projeto
+        self.valor = 1
 
     def requisicao(self):
         reposta = requests.get(
-            f'https://api.github.com/repos/{}'
+            f'https://api.github.com/repos/{self.dono}/{self.projeto}/contributors?per_page=100&page={self.valor}',
+            headers=headers
         )
+
+        if reposta.status_code == 200:
+            return reposta.json()
+        else:
+            return reposta.status_code
+
+    def gravacao(self):
+        dados = self.requisicao()
+
+        if type(dados) is not int:
+            with open("usuarios.csv", "a") as f:
+                while dados:
+                    for i in range(len(dados)):
+                        f.write(dados[i]['login'] + "," + str(dados[i]['contributions']) + "," + self.projeto + "\n")
+
+                    self.valor += 1
+                    dados = self.requisicao()
