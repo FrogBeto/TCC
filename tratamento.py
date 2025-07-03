cf2dc9e57f1fd2888e5b44df459aa77adee1b340
FrogBeto <albertoazevedo112@gmail.com>
2025-07-03 20:40:06 -0300

    Testes Localização

--- tratamento.py
+++ tratamento.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import requests
 
-token = ""
-headers = {
-    "Authorization": "access_token {}".format(token)
-}
-
 df = pd.read_csv('dados2.csv', sep=';')
+aux = pd.read_csv('filtro.csv', sep=';')
 locais = []
+filtro = []
+
+flag = True
 
 for x in df['location']:
     locais.append(x)
+for x in aux['location']:
+    filtro.append(x)
 
 class Tratamento:
     def __init__(self, localizacao):
@@ -18,8 +19,7 @@ class Tratamento:
 
     def requisicao(self):
         reposta = requests.get(
-            f'https://api.mapbox.com/search/geocode/v6/forward?q={self.localizacao}&limit=1',
-            headers=headers
+            f'https://api.mapbox.com/search/geocode/v6/forward?q={self.localizacao}&access_token='
         )
 
         return reposta.json()
@@ -29,5 +29,28 @@ class Tratamento:
 
         with open("dados3.csv", "a", encoding="utf-8") as f:
             f.write(
-                self.localizacao + "\n"
+                dados['features'][0]['properties']['context']['country']['name'] + "\n"
             )
+
+for x in range(5):
+    for y in range(len(filtro)):
+        if locais[x] == filtro[y]:
+            with open("dados3.csv", "a", encoding="utf-8") as f:
+                f.write(
+                    "<null>" + "\n"
+                )
+
+            flag = False
+
+            break
+    if locais[x] is None:
+        print(locais[x])
+    else:
+        print("locais[x]")
+
+    if flag:
+        #local = Tratamento(locais[x])
+        #local.gravacao()
+        print("\n")
+    else:
+        flag = True
