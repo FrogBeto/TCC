6d789c6a702c5ee6645cd3729b9325b4ba384b64
FrogBeto <albertoazevedo112@gmail.com>
2025-08-04 19:20:16 -0300

    Tratamento inicial

--- tratamento.py
+++ tratamento.py
@@ -2,16 +2,12 @@ import pandas as pd
 import requests
 
 df = pd.read_csv('dados2.csv', sep=';')
-aux = pd.read_csv('filtro.csv', sep=';')
 locais = []
-filtro = []
-
+y = 6775
 flag = True
 
 for x in df['location']:
     locais.append(x)
-for x in aux['location']:
-    filtro.append(x)
 
 class Tratamento:
     def __init__(self, localizacao):
@@ -19,7 +15,7 @@ class Tratamento:
 
     def requisicao(self):
         reposta = requests.get(
-            f'https://api.mapbox.com/search/geocode/v6/forward?q={self.localizacao}&access_token='
+            f'https://api.mapbox.com/search/geocode/v6/forward?q={self.localizacao}&access_token=pk.eyJ1IjoiZnJvZ2JldG8iLCJhIjoiY21ja3dyd2FhMDR0dTJrcTFsOWJ4bmtpbCJ9.2NqjRFjSstc5gHSpmZctyw'
         )
 
         return reposta.json()
@@ -30,29 +26,32 @@ class Tratamento:
         with open("dados3.csv", "a", encoding="utf-8") as f:
             f.write(
                 dados['features'][0]['properties']['context']['country']['name'] + ";" +
-                dados['features'][0]['properties']['coordinates']['longitude'] + ";" +
-                dados['features'][0]['properties']['coordinates']['latitude'] + "\n"
+                str(dados['features'][0]['properties']['coordinates']['longitude']) + ";" +
+                str(dados['features'][0]['properties']['coordinates']['latitude']) + "\n"
             )
 
-for x in range(5):
-    for y in range(len(filtro)):
-        if locais[x] == "filtro[y]":
-            with open("dados3.csv", "a", encoding="utf-8") as f:
-                f.write(
-                    "<null>" + ";" + "0" + ";" + "0" + "\n"
-                )
+for x in range(len(locais)):
+    x = y
+    if locais[x] == "<null>":
+        with open("dados3.csv", "a", encoding="utf-8") as f:
+            f.write(
+                "<null>" + ";" + "0" + ";" + "0" + "\n"
+            )
 
-            flag = False
+        flag = False
+    elif locais[x] == "Europe":
+        with open("dados3.csv", "a", encoding="utf-8") as f:
+            f.write(
+                "Europe" + ";" + "7.418962" + ";" + "43.732482" + "\n"
+            )
 
-            break
-    if locais[x] is None:
-        print(locais[x])
-    else:
-        print("locais[x]")
+        flag = False
 
     if flag:
-        #local = Tratamento(locais[x])
-        #local.gravacao()
-        print("\n")
+        local = Tratamento(locais[x])
+        local.gravacao()
+        print("{:.2f}".format((x+1)/11610 * 100) + "% " + str(x+1) + " " + locais[x])
     else:
         flag = True
+
+    y += 1
