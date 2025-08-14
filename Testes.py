fab996680cbd37a0c3488e5456dfbfd52c64b531
FrogBeto <albertoazevedo112@gmail.com>
2025-08-14 17:57:36 -0300

    Beta do Mapa Geral

--- Testes.py
+++ Testes.py
@@ -1,10 +1,31 @@
 import pandas as pd
+import folium
 
 df = pd.read_csv('CSVs/Dados Finais/dados.csv', sep=';')
 
-teste = df.groupby(['Projeto', 'Longitude', 'Latitude']).size().reset_index(name='Quantidade').sort_values('Quantidade', ascending=False).reset_index(drop=True)
+teste = df.groupby(['Projeto', 'Longitude', 'Latitude']).size().reset_index(name='Quantidade').sort_values('Projeto', ascending=False).reset_index(drop=True)
 
 teste2 = teste.groupby(['Projeto', 'Quantidade']).mean().reset_index()
 teste3 = []
-for x in teste2
+for x in range(len(teste2)):
+    print(teste2['Projeto'][x], teste2['Quantidade'][x])
+
+m = folium.Map()
+
+fg = folium.FeatureGroup(name="Icon collection", show=False).add_to(m)
+folium.Marker(location=(0, 0), icon=folium.Icon(color="red")).add_to(fg)
+folium.Marker(location=(1, 1), icon=folium.Icon(color="red")).add_to(fg)
+folium.Marker(location=(2, 2), icon=folium.Icon(color="green")).add_to(fg)
+folium.Marker(location=(3, 3), icon=folium.Icon(color="green")).add_to(fg)
+
+fg = folium.FeatureGroup(name="Teste do Teste", show=False).add_to(m)
+folium.Marker(location=(2, 2), icon=folium.Icon(color="green")).add_to(fg)
+folium.Marker(location=(3, 3), icon=folium.Icon(color="green")).add_to(fg)
+
+
+folium.LayerControl().add_to(m)
+
+m.save('Mapas/Teste.html')
+
+print(teste)
 print(teste2)
--- mapa.py
+++ mapa.py
@@ -3,7 +3,9 @@ import folium
 
 df = pd.read_csv('CSVs/Dados Finais/dados.csv', sep=';')
 
-pontos = df.groupby(['Longitude', 'Latitude']).size().reset_index(name='Quantidade').sort_values('Quantidade', ascending=False).reset_index(drop=True)
+pontosCalor = df.groupby(['Longitude', 'Latitude']).size().reset_index(name='Quantidade').sort_values('Quantidade', ascending=False).reset_index(drop=True)
+pontosProjetos = df.groupby(['Projeto', 'Longitude', 'Latitude']).size().reset_index(name='Quantidade').sort_values('Projeto', ascending=False).reset_index(drop=True)
+quantProjetos = pontosProjetos.groupby(['Projeto', 'Quantidade']).mean().reset_index()
 
 m = folium.Map(
     [0,0],
@@ -18,7 +20,7 @@ class Mapa:
         self.Latitude = latitude
         self.Quantidade = quantidade
 
-    def adicionar(self):
+    def adicionar_calor(self):
         folium.CircleMarker(
             location=[self.Latitude, self.Longitude],
             radius=(10 + int(self.Quantidade / 5)),
@@ -26,15 +28,39 @@ class Mapa:
             stroke=False,
             fill=True,
             fill_opacity=(0.31 + (0.01 * self.Quantidade)),
-        ).add_to(m)
+        ).add_to(fg)
 
-for x in range(len(pontos)):
+    def adicionar_ponto(self):
+        folium.CircleMarker(
+            location=[self.Latitude, self.Longitude],
+            radius=(10 + self.Quantidade),
+            color='green',
+            stroke=False,
+            fill=True,
+            fill_opacity=(0.30 + (0.07 * self.Quantidade)),
+        ).add_to(fg)
+
+fg = folium.FeatureGroup(name='Mapa de Calor', show=True).add_to(m)
+for x in range(len(pontosCalor)):
     aux = 0
-    for y in pontos.groupby('Quantidade').mean().reset_index().sort_values('Quantidade', ascending=True).reset_index(drop=True)['Quantidade']:
-        if pontos['Quantidade'][x] == y:
-            ponto = Mapa(pontos['Longitude'][x], pontos['Latitude'][x], aux)
-            ponto.adicionar()
+    for y in pontosCalor.groupby('Quantidade').mean().reset_index().sort_values('Quantidade', ascending=True).reset_index(drop=True)['Quantidade']:
+        if pontosCalor['Quantidade'][x] == y:
+            ponto = Mapa(pontosCalor['Longitude'][x], pontosCalor['Latitude'][x], aux)
+            ponto.adicionar_calor()
         else:
             aux = aux + 1
 
+for x in range(len(pontosProjetos)):
+    aux = 0
+    if x == 0 or pontosProjetos['Projeto'][x] != pontosProjetos['Projeto'][x-1]:
+        fg = folium.FeatureGroup(name=pontosProjetos['Projeto'][x], show=False).add_to(m)
+
+    for y in range(len(quantProjetos)):
+        if pontosProjetos['Quantidade'][x] == quantProjetos['Quantidade'][y] and pontosProjetos['Projeto'][x] == quantProjetos['Projeto'][y]:
+            ponto = Mapa(pontosProjetos['Longitude'][x], pontosProjetos['Latitude'][x], aux)
+            ponto.adicionar_ponto()
+        elif pontosProjetos['Projeto'][x] == quantProjetos['Projeto'][y]:
+            aux = aux + 1
+
+folium.LayerControl().add_to(m)
 m.save('Mapas/mapa.html')
